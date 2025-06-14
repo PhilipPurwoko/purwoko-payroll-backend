7973c03999fd81a4312f58e22ef68bd4392feda9
Philip Purwoko <philippurwoko123@gmail.com>
2025-06-14 19:44:01 +0700

    feat: add swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Purwoko Payroll API')
+    .setDescription('API Docs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
+
 bootstrap();
