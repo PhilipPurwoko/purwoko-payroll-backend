a6ca88a8befb458b68bec190107f6c2f817a7480
Philip Purwoko <philippurwoko123@gmail.com>
2025-06-15 10:26:44 +0700

    fix: api configure attendance for admin user

--- src/modules/attendance_configuration/attendance_configuration.controller.ts
+++ src/modules/attendance_configuration/attendance_configuration.controller.ts
@@ -6,10 +6,15 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { AttendanceConfigurationService } from './attendance_configuration.service';
 import { CreateAttendanceConfigurationDto } from './dto/create-attendance_configuration.dto';
 import { UpdateAttendanceConfigurationDto } from './dto/update-attendance_configuration.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { RoleGuard } from '../auth/guard/role.guard';
+import { Roles } from '../auth/decorator/role.decorator';
 
 @Controller('attendance-configuration')
 export class AttendanceConfigurationController {
@@ -17,6 +22,9 @@ export class AttendanceConfigurationController {
     private readonly attendanceConfigurationService: AttendanceConfigurationService,
   ) {}
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles('admin')
   @Post()
   create(
     @Body() createAttendanceConfigurationDto: CreateAttendanceConfigurationDto,
@@ -26,16 +34,25 @@ export class AttendanceConfigurationController {
     );
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles('admin')
   @Get()
   findAll() {
     return this.attendanceConfigurationService.findAll();
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles('admin')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.attendanceConfigurationService.findOne(id);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles('admin')
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -47,6 +64,9 @@ export class AttendanceConfigurationController {
     );
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles('admin')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.attendanceConfigurationService.remove(id);
