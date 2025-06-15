781e56aa6ac2758ae72636fd756779f0bfb616ff
Philip Purwoko <philippurwoko123@gmail.com>
2025-06-15 10:20:21 +0700

    feat: add user seeder

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -2,18 +2,25 @@ import * as dotenv from 'dotenv';
 import * as bcrypt from 'bcrypt';
 import { v4 } from 'uuid';
 import { PrismaClient, User } from '@prisma/client';
+import { convertToEmail, getRandomName } from './name';
 
 dotenv.config();
 
 const prisma: PrismaClient = new PrismaClient();
 
 async function main() {
+  console.log('[SEEDER] START');
+
+  const round = parseInt(process.env.SALT_ROUNDS || '10');
+  const masterPassword = process.env.MASTER_PASSWORD || 'password';
+  const masterHashedPassword = bcrypt.hashSync(masterPassword, round);
+
   const users: User[] = [
     {
       id: v4(),
       name: 'admin',
       email: 'admin@purwoko.dev',
-      password: process.env.MASTER_PASSWORD || 'password',
+      password: masterHashedPassword,
       role: 'admin',
       isActive: true,
       createdAt: new Date(),
@@ -24,17 +31,31 @@ async function main() {
     },
   ];
 
-  const round = parseInt(process.env.SALT_ROUNDS || '10');
-  for (const user of users) {
-    const { password, ...rest } = user;
+  for (let i = 0; i < 100; i++) {
+    const id = v4();
+    const name = getRandomName();
+    const email = convertToEmail(name);
+    const password = process.env.SEEDER_PASSWORD || 'password';
     const hashedPassword = bcrypt.hashSync(password, round);
-    await prisma.user.create({
-      data: {
-        ...rest,
-        password: hashedPassword,
-      },
+    users.push({
+      id: id,
+      name: name,
+      email: email,
+      password: hashedPassword,
+      role: 'employee',
+      isActive: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+      createdBy: null,
+      deletedBy: null,
     });
   }
+
+  await prisma.user.createMany({
+    data: users,
+  });
+  console.log('[SEEDER] END');
 }
 
 main()
